b5bb72bacc2b76b0ec514c22ecf8781d3bfe78b5
Yiang <1712064922@qq.com>
2022-10-17 23:26:33 +0800

    配置文件入门

--- p01_hello/src/main/java/com/atguigu/controller/HelloController.java
+++ p01_hello/src/main/java/com/atguigu/controller/HelloController.java
@@ -1,12 +1,17 @@
 package com.atguigu.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	@Value("${server.port}")
+	private String port;
+
 	@RequestMapping("/hello")
 	public String sayHello() {
+		System.out.println("port = " + port);
 		return "hello spring boot!!";
 	}
 }
