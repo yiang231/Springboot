2b48cdaa448bcbb37456748935d0b8c268d6ef07
Yiang <1712064922@qq.com>
2022-10-19 08:38:32 +0800

    综合案例：引入actuator,给综合案例添加actuator

---
+++ p05_actuator/src/main/java/cm/atguigu/Application5.java
@@ -0,0 +1,11 @@
+package cm.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application5 {
+	public static void main(String[] args) {
+		SpringApplication.run(Application5.class, args);
+	}
+}
